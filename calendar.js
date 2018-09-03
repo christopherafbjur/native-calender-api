0af88e836c4f66e0c6d72e0124d1cd12148af115
Christopher Af Bjur <cabc@christophersmbp.mynet>
2018-09-03 12:31:13 +0200

    Bugfix: Fixed a bug related to date validation

--- calendar.js
+++ calendar.js
@@ -1,28 +1,29 @@
-const P = require('bluebird');
-const moment = require('moment');
+const P = require("bluebird");
+const moment = require("moment");
 
 class NativeCalendar {
   setup(titlePrefix, errorPrefix) {
     if (window.plugins && window.plugins.calendar) {
       this.calendar = window.plugins.calendar;
     } else {
-      console.warn('this package is dependant on the calendar plugin please call setup After it has loaded');
+      console.warn(
+        "this package is dependant on the calendar plugin please call setup After it has loaded"
+      );
     }
     this.errorPrefix = errorPrefix;
     this.titlePrefix = titlePrefix;
-    this.errors = '';
+    this.errors = "";
   }
 
   clearErrors() {
-    this.errors = '';
+    this.errors = "";
   }
 
   validDate(date) {
     if (Object.prototype.toString.call(date) === "[object Date]") {
-      if (isNaN(d.getTime())) {
+      if (isNaN(date.getTime())) {
         return false;
-      }
-      else {
+      } else {
         return true;
       }
     }
@@ -31,24 +32,24 @@ class NativeCalendar {
 
   isValid(event) {
     let valid = true;
-    ['location', 'title', 'notes'].forEach((prop) => {
+    ["location", "title", "notes"].forEach(prop => {
       if (!event[prop]) {
-        this.errors += `${this.errorPrefix} ${prop} was not valid.\n`
+        this.errors += `${this.errorPrefix} ${prop} was not valid.\n`;
         valid = false;
       }
-      if (typeof(event[prop]) !== 'string') {
+      if (typeof event[prop] !== "string") {
         valid = false;
-        this.errors += `${this.errorPrefix} ${prop} was not of type string.\n`
+        this.errors += `${this.errorPrefix} ${prop} was not of type string.\n`;
       }
     });
 
-    ['startDate', 'endDate'].forEach((prop) => {
+    ["startDate", "endDate"].forEach(prop => {
       if (!event[prop]) {
         valid = false;
-        this.errors += `${this.errorPrefix} ${prop} was not truthy.\n`
-      } else if(!this.validDate(event[prop])) {
+        this.errors += `${this.errorPrefix} ${prop} was not truthy.\n`;
+      } else if (!this.validDate(event[prop])) {
         valid = false;
-        this.errors += `${this.errorPrefix} ${prop} was not a valid date.\n`
+        this.errors += `${this.errorPrefix} ${prop} was not a valid date.\n`;
       }
     });
 
@@ -62,7 +63,9 @@ class NativeCalendar {
   }
 
   newEvent(event) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return this.asyncCreateEvent(event).then(() => {
       return P.resolve();
@@ -70,94 +73,150 @@ class NativeCalendar {
   }
 
   deleteEvent(event) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return this.asyncDeleteEvent(job).then(() => {
       return P.resolve();
     });
   }
 
-  convertFindEventToDeleteEvent(
-    {
-      title = '',
-      notes = '',
-      location = '',
-      startDate = moment().toDate(),
-      endDate = moment().toDate(),
-    }
-  ) {
+  convertFindEventToDeleteEvent({
+    title = "",
+    notes = "",
+    location = "",
+    startDate = moment().toDate(),
+    endDate = moment().toDate()
+  }) {
     return {
       title,
       notes,
       location,
       startDate: this.convertFindDateToJavaScriptDate(startDate),
-      endDate: this.convertFindDateToJavaScriptDate(endDate),
-    }
+      endDate: this.convertFindDateToJavaScriptDate(endDate)
+    };
   }
 
   convertFindDateToJavaScriptDate(date) {
-    return moment(date, 'YYYY-MM-DD HH:mm:ss').toDate();
+    return moment(date, "YYYY-MM-DD HH:mm:ss").toDate();
   }
 
   getDefaultBlankObject() {
     return {
-      title: '',
-      notes: '',
-      location: '',
+      title: "",
+      notes: "",
+      location: "",
       startDate: moment().toDate(),
-      endDate: moment().toDate(),
+      endDate: moment().toDate()
     };
   }
 
   asyncCreateEvent(event) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.createEvent(title, location, notes, startDate, endDate, resolve, reject);
+      this.calendar.createEvent(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        resolve,
+        reject
+      );
     });
   }
 
   asyncCreateEventWithOptions(event, options) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.createEventWithOptions(title, location, notes, startDate, endDate, options, resolve, reject);
+      this.calendar.createEventWithOptions(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        options,
+        resolve,
+        reject
+      );
     });
   }
 
   asyncCreateEventInteractively(event) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.createEventInteractively(title, location, notes, startDate, endDate, resolve, reject);
+      this.calendar.createEventInteractively(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        resolve,
+        reject
+      );
     });
   }
 
   asyncCreateEventInteractivelyWithOptions(event, options) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.createEventInteractivelyWithOptions(title, location, notes, startDate, endDate, options, resolve, reject);
+      this.calendar.createEventInteractivelyWithOptions(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        options,
+        resolve,
+        reject
+      );
     });
   }
 
   asyncFindEvent(event) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.findEvent(title, location, notes, startDate, endDate, resolve, reject);
+      this.calendar.findEvent(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        resolve,
+        reject
+      );
     });
   }
 
   asyncDeleteEventById(id) {
-    return this.asyncFindEventWithOptions(this.getDefaultBlankObject(), { id }).then((events) => {
+    return this.asyncFindEventWithOptions(this.getDefaultBlankObject(), {
+      id
+    }).then(events => {
       const event = this.convertFindEventToDeleteEvent(events[0]);
-      if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+      if (!this.isValid(event)) {
+        return this.getErrorsAsReject();
+      }
 
       return this.asyncDeleteEvent(event);
     });
@@ -166,14 +225,31 @@ class NativeCalendar {
   asyncFindEventWithOptions(event, options) {
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.findEventWithOptions(title, location, notes, startDate, endDate, options, resolve, reject);
+      this.calendar.findEventWithOptions(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        options,
+        resolve,
+        reject
+      );
     });
   }
 
   asyncDeleteEvent(event) {
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.deleteEvent(title, location, notes, startDate, endDate, resolve, reject);
+      this.calendar.deleteEvent(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        resolve,
+        reject
+      );
     });
   }
 
@@ -184,11 +260,22 @@ class NativeCalendar {
   }
 
   asyncDeleteEventFromNamedCalendar(event, calendarName) {
-    if (!this.isValid(event)) { return this.getErrorsAsReject(); }
+    if (!this.isValid(event)) {
+      return this.getErrorsAsReject();
+    }
 
     return new P((resolve, reject) => {
       const { title, location, notes, startDate, endDate } = event;
-      this.calendar.deleteEventFromNamedCalendar(title, location, notes, startDate, endDate, calendarName, resolve, reject);
+      this.calendar.deleteEventFromNamedCalendar(
+        title,
+        location,
+        notes,
+        startDate,
+        endDate,
+        calendarName,
+        resolve,
+        reject
+      );
     });
   }
 }
